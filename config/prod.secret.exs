afa7c5037e0e78c5b9b98d82055f92a03b037bdc
Yexin Wang <wang.yexin@outlook.com>
2019-11-09 00:58:18 -0500

    add config file

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -36,3 +36,4 @@ config :timesheets_spa, TimesheetsSpaWeb.Endpoint,
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
+config :timesheets_spa, TimesheetsSpaWeb.Endpoint, server: true
